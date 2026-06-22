81601aa12304711d5ed860253af34a49d68f270a
Aleksandr Babynin <babynin.home@gmail.com>
2026-06-23 00:38:47 +1100

    Send random quiz questions

--- echo_bot.py
+++ echo_bot.py
@@ -1,14 +1,18 @@
 import os
+from random import choice
 
 from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+
+from quiz_parser import load_quiz_questions
 
 
 QUIZ_KEYBOARD = [
     ["Новый вопрос", "Сдаться"],
     ["Мой счёт"],
 ]
+NEW_QUESTION_BUTTON = "Новый вопрос"
 
 
 def get_keyboard():
@@ -22,7 +26,19 @@ def start(update, context):
     )
 
 
-def echo(update, context):
+def send_new_question(update, context):
+    question = choice(context.bot_data["question_texts"])
+    update.message.reply_text(
+        question,
+        reply_markup=get_keyboard(),
+    )
+
+
+def handle_message(update, context):
+    if update.message.text == NEW_QUESTION_BUTTON:
+        send_new_question(update, context)
+        return
+
     update.message.reply_text(
         update.message.text,
         reply_markup=get_keyboard(),
@@ -36,11 +52,17 @@ def run_bot():
     if not telegram_token:
         raise RuntimeError("Set TG_TOKEN in .env")
 
+    quiz_questions = load_quiz_questions()
+    if not quiz_questions:
+        raise RuntimeError("Questions were not found")
+
     updater = Updater(telegram_token)
     dispatcher = updater.dispatcher
+    dispatcher.bot_data["quiz_questions"] = quiz_questions
+    dispatcher.bot_data["question_texts"] = list(quiz_questions.keys())
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
     updater.start_polling()
     updater.idle()
